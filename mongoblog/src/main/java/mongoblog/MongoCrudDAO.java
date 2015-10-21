d52f2b952ce49c3e36124bd01f8fd834b2e6612f
kyle-reese <joeyfaherty@live.ie>
2015-10-21 21:01:03 +0100

    find all documents in a collection  of certain criteria

--- mongoblog/src/main/java/mongoblog/MongoCrudDAO.java
+++ mongoblog/src/main/java/mongoblog/MongoCrudDAO.java
@@ -1,9 +1,12 @@
 package mongoblog;
 
-import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.*;
+
+import java.util.List;
 
 import org.bson.Document;
 
+import com.mongodb.Block;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
@@ -18,14 +21,22 @@ public class MongoCrudDAO {
 		MongoDatabase database = mongoClient.getDatabase("students");
 		
 		// specify collection name 
-		MongoCollection<Document> collection = database.getCollection("xxxxtest");  //grades
+		MongoCollection<Document> collection = database.getCollection("grades"); 
 		
-		//Finds all documents in the collection
-		Document doc = collection.find(eq("elementNo", 71)).first();
-		System.out.println(doc.toJson());
+//		//Finds all documents in the collection
+//		Document doc = collection.find(eq("elementNo", 71)).first();
+//		System.out.println(doc.toJson());
+//		
+//		//Atomically find a document and remove it.
+//		List<Document> docs = collection.find(eq("elementNo", "123"));
 		
-		//Atomically find a document and remove it.
-		collection.findOneAndDelete(eq("elementNo", 71));
+		// now use a range query to get a larger subset
+		Block<Document> printBlock = new Block<Document>() {
+		     public void apply(final Document document) {
+		         System.out.println(document.toJson());
+		     }
+		};
+		collection.find(eq("type", "homework")).forEach(printBlock);
 		
 		//close the connection
 		mongoClient.close();
